5e402a69c544ac910df43cc1464d5a6608931bcb
Jason Smyth <jason.s.smyth@gmail.com>
2023-03-14 14:58:17 -0700

    Finished Lab08 Part 4

--- Lab8_2023/ContentView.swift
+++ Lab8_2023/ContentView.swift
@@ -7,24 +7,18 @@
 
 import SwiftUI
 import RealityKit
-
-struct ContentView : View {
-    var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
-    }
-}
-
 struct ARViewContainer: UIViewRepresentable {
+    let WorldAnchor: Experience.World
     
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(WorldAnchor)
         
         return arView
         
@@ -33,6 +27,24 @@ struct ARViewContainer: UIViewRepresentable {
     func updateUIView(_ uiView: ARView, context: Context) {}
     
 }
+
+struct ContentView : View {
+    let WorldAnchor = try! Experience.loadWorld()
+    var body: some View {
+        
+        
+        ARViewContainer(WorldAnchor: WorldAnchor).edgesIgnoringSafeArea(.all)
+        
+        VStack{
+            Button("Orbit"){
+                WorldAnchor.notifications.orbitChess.post()
+        }
+        
+        }
+    }
+}
+
+
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
